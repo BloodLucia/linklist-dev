20e2b778087ccdfb2ee4153e3cafb3fa821798ed
zwhy2024 <thecvcoder@foxmail.com>
2024-06-18 03:13:29 +0800

    delete:page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export const metadata = {
-  title: 'Oh My Link',
-  description: 'Oh My Link',
-}
-export default async function Home() {
-  return redirect('/dashboard/links')
-}
