db5a7930160d8e66be74954a28c0a885232004c2
zwhy2024 <thecvcoder@foxmail.com>
2024-06-18 23:30:16 +0800

    middleware:testing........

--- middleware.ts
+++ middleware.ts
@@ -29,7 +29,8 @@ export async function middleware(request: NextRequest) {
     )
   }
 
-  return await updateSession(request)
+  // return await updateSession(request)
+  return response
 }
 
 export const config = {
@@ -43,6 +44,6 @@ export const config = {
      * Feel free to modify this pattern to include more paths.
      */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-    '/dashboard/(.*)'
+    '/dashboard/(.*)',
   ],
 }
