bef3b13fae8d42d152af723e535521e0ddfde8c6
zwhy2024 <thecvcoder@foxmail.com>
2024-06-19 16:56:53 +0800

    config tailwindcss primarycolor

--- components/Layout/SiteHeader.tsx
+++ components/Layout/SiteHeader.tsx
@@ -11,21 +11,6 @@ export const SiteHeader: React.FC<{ logged?: boolean }> = ({
   return (
     <header className="fixed top-0 left-0 right-0 z-10 border-b border-b-[#dee2e6] box-border bg-[#f9f9f9] py-2">
       <div className="xl:max-w-6xl mx-auto px-4 flex justify-between items-center max-md:w-full h-full">
-        {/* <Link
-          href="/"
-          className={`inline-flex gap-x-3 items-center text-xl ${font.className}`}
-        >
-          <Image
-            className="align-middle"
-            src="/logo.svg"
-            width={32}
-            height={32}
-            alt="LinkList"
-            decoding="auto"
-            loading="eager"
-          />
-          LinkList
-        </Link> */}
         <Brand />
       </div>
     </header>
