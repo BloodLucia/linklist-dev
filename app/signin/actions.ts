c415adc7de6dfd8b707782ec6a4a424dc54ca717
zwhy2024 <thecvcoder@foxmail.com>
2024-06-19 19:28:54 +0800

    create sign action use supabase client

--- app/signin/actions.ts
+++ app/signin/actions.ts
@@ -1,14 +1,45 @@
 'use server'
 
+import { getToastRedirect } from '@/utils/helpers/helpers'
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
-import { cache } from 'react'
 
-export const signInWithPasswordAndUserName = (formData: FormData) => {
+export const signInWithPasswordAndUserName = async (formData: FormData) => {
   const pathname = String(formData.get('pathname')).trim()
   const username = String(formData.get('username')).trim()
   const password = String(formData.get('password')).trim()
   const supabase = createClient()
   const cookieStore = cookies()
   let redirectURL: string
+  const { data, error } = await supabase
+    .from('users')
+    .select('*')
+    .eq('username', username)
+    .maybeSingle()
+
+  if (error) {
+    redirectURL = getToastRedirect(pathname, 'error', error.message)
+  }
+
+  if (data) {
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.signInWithPassword({
+      email: data.email,
+      password,
+    })
+    if (error) {
+      redirectURL = getToastRedirect(pathname, 'error', error.message)
+    } else if (user) {
+      cookieStore.set('preferredSignInView', 'password_signin', { path: '/' })
+      redirectURL = getToastRedirect('/dashboard', 'status', '登录成功!')
+    } else {
+      redirectURL = getToastRedirect(pathname, 'error', '服务器内部发生了错误')
+    }
+  } else {
+    redirectURL = getToastRedirect(pathname, 'error', '用户名或密码错误')
+  }
+
+  return redirectURL
 }
--- components/Forms/PasswordSignIn.tsx
+++ components/Forms/PasswordSignIn.tsx
@@ -1,20 +1,20 @@
 'use client'
 
 import { handleRequest } from '@/utils/supabase/auth-helpers/client'
-import { signInWithUsernameAndPassword } from '@/utils/supabase/auth-helpers/server'
 import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { PasswordInput } from '../Inputs/PasswordInput'
 import { Input } from '../Inputs/Input'
-import Image from 'next/image'
 import { Button } from '../Buttons/Button'
+import { signInWithPasswordAndUserName } from '@/app/signin/actions'
+import Image from 'next/image'
 
 export const PasswordSignIn = () => {
   const router = useRouter()
   const [isLoading, setIsLoading] = useState(false)
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     setIsLoading(true)
-    await handleRequest(e, signInWithUsernameAndPassword, router)
+    await handleRequest(e, signInWithPasswordAndUserName, router)
     setIsLoading(false)
   }
   return (
@@ -30,7 +30,8 @@ export const PasswordSignIn = () => {
         priority
         className="mx-auto mb-6 cursor-pointer"
       />
-      <Input name="username" placeholder="用户名" required autoComplete="off" />
+      <Input name="username" placeholder="用户名" required autoComplete="username" />
+      <input type="hidden" name="pathname" defaultValue={usePathname()} />
       <PasswordInput />
       <Button type="submit" className="mt-4" disabled={isLoading}>
         登录
