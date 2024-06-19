3bca3ddecaa1a8b4c49d2a3a6e8e79a1e70bb8e6
zwhy2024 <thecvcoder@foxmail.com>
2024-06-19 20:13:06 +0800

    UrlInput validation

--- app/signin/actions.ts
+++ app/signin/actions.ts
@@ -1,8 +1,93 @@
 'use server'
 
-import { getToastRedirect } from '@/utils/helpers/helpers'
+import { getToastRedirect, getURL } from '@/utils/helpers/helpers'
 import { createClient } from '@/utils/supabase/server'
+import { validate } from 'email-validator'
 import { cookies } from 'next/headers'
+
+export const existsByUserName = async (username: string) => {
+  const supabase = createClient()
+  const data = (
+    await supabase.from('profiles').select('*').eq('username', username)
+  ).data
+  return !!data && data.length > 0
+}
+
+export const signUp = async (formData: FormData) => {
+  const callbackURL = getURL('/auth/callback')
+  const supabase = createClient()
+  const email = String(formData.get('email')).trim()
+  const password = String(formData.get('password')).trim()
+  const username = String(formData.get('username')).trim()
+  const pathname = String(formData.get('pathname')).trim()
+  let redirectPath: string
+
+  if (!validate(email)) {
+    redirectPath = getToastRedirect(
+      '/signin/signup',
+      'error',
+      'Invalid email address.'
+    )
+  }
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: callbackURL,
+    },
+  })
+
+  if (error) {
+    console.log(error.code)
+    redirectPath = getToastRedirect(pathname, 'error', error.message)
+  } else if (data.session) {
+    // save a user to database
+    const { data, error } = await supabase
+      .from('users')
+      .insert({ username, email })
+      .select()
+    if (!error && data.length > 0 && data[0].id > 0) {
+      redirectPath = getToastRedirect(
+        '/setup-your-page',
+        'status',
+        'You are now signed in.'
+      )
+    } else if (error) {
+      redirectPath = getToastRedirect(pathname, 'error', error?.message)
+    } else {
+      redirectPath = getToastRedirect(
+        pathname,
+        'error',
+        'Internal Server Error.'
+      )
+    }
+  } else if (
+    data.user &&
+    data.user.identities &&
+    data.user.identities.length == 0
+  ) {
+    redirectPath = getToastRedirect(
+      pathname,
+      'error',
+      'There is already an account associated with this email address. Try resetting your password.'
+    )
+  } else if (data.user) {
+    redirectPath = getToastRedirect(
+      '/dashboard',
+      'status',
+      'Please check your email for a confirmation link. You may now close this tab.'
+    )
+  } else {
+    redirectPath = getToastRedirect(
+      pathname,
+      'error',
+      'You could not be signed up.'
+    )
+  }
+
+  return redirectPath
+}
 
 export const signInWithPasswordAndUserName = async (formData: FormData) => {
   const pathname = String(formData.get('pathname')).trim()
--- components/Inputs/UrlInout.tsx
+++ components/Inputs/UrlInout.tsx
@@ -1,4 +1,11 @@
+'use client'
+
+import { useDebounce } from '@uidotdev/usehooks'
+import cn from 'classnames'
+
 import s from './Input.module.css'
+import { useEffect, useState } from 'react'
+import { existsByUserName } from '@/app/signin/actions'
 
 interface Props {
   name?: string
@@ -10,6 +17,22 @@ export const UrlInput: React.FC<Props> = ({
   placeholder = 'username',
   onChange,
 }) => {
+  const [username, setUsername] = useState('')
+  const [isValid, setIsValid] = useState(false)
+  const debounceSearcher = useDebounce(username, 300)
+
+  useEffect(() => {
+    ;(async () => {
+      if (debounceSearcher) {
+        setIsValid(await existsByUserName(username))
+      } else {
+        setIsValid(false)
+      }
+    })()
+  }, [debounceSearcher])
+  const className = cn(s['input'], {
+    [s['error']]: isValid,
+  })
   return (
     <div className={s['wrapper']}>
       <input
@@ -17,8 +40,8 @@ export const UrlInput: React.FC<Props> = ({
         name={name}
         required
         placeholder={placeholder}
-        onChange={onChange}
-        className={s['input']}
+        onChange={(e) => setUsername(e.target.value.trim())}
+        className={className}
         autoComplete="off"
         maxLength={20}
         minLength={4}
@@ -26,6 +49,11 @@ export const UrlInput: React.FC<Props> = ({
       <div className="absolute top-[50%] -translate-y-[50%] left-4 text-textDark">
         linklist.one/
       </div>
+      {isValid && (
+        <div className="text-danger text-xs absolute -bottom-3 leading-none">
+          用户名已存在
+        </div>
+      )}
     </div>
   )
 }
