2992e9d4720bb11c03288c4ee02dc9cd5aea1740
zwhy2024 <thecvcoder@foxmail.com>
2024-06-24 20:53:54 +0800

    update: use PathnameInput

--- components/Forms/PasswordSignIn.tsx
+++ components/Forms/PasswordSignIn.tsx
@@ -1,13 +1,14 @@
 'use client'
 
-import { handleRequest } from '@/utils/supabase/auth-helpers/client'
+import Image from 'next/image'
 import { useState } from 'react'
-import { usePathname, useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation'
+import { handleRequest } from '@/utils/supabase/auth-helpers/client'
 import { PasswordInput } from '../Inputs/PasswordInput'
 import { Input } from '../Inputs/Input'
 import { Button } from '../Buttons/Button'
 import { signInWithPasswordAndUserName } from '@/app/signin/actions'
-import Image from 'next/image'
+import { PathnameInput } from '../Inputs/PathnameInput'
 
 export const PasswordSignIn = () => {
   const router = useRouter()
@@ -31,12 +32,7 @@ export const PasswordSignIn = () => {
         className="mx-auto mb-6 cursor-pointer"
       />
       <Input name="username" placeholder="用户名" required autoComplete="off" />
-      <input
-        type="hidden"
-        name="pathname"
-        defaultValue={usePathname()}
-        autoComplete="off"
-      />
+      <PathnameInput />
       <PasswordInput />
       <Button type="submit" className="mt-4" loading={isLoading}>
         登录
